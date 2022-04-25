7bda5a219200e256b32ddebb7de379deb0572fe3
crus <romanghiet05@gmail.com>
2022-04-25 01:15:18 -0700

    begin Project

---
+++ Client-App/Client-App/Client_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Client_AppApp.swift
+//  Client-App
+//
+//  Created by crus on 24.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Client_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
