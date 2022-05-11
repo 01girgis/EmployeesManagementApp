e3c7d9ab7857bd67dc753cf02b0a24a073a9af82
crus <romanghiet05@gmail.com>
2022-05-11 00:39:21 -0700

    build APP ui

--- Client-App/Client-App/View/MainView.swift
+++ Client-App/Client-App/View/MainView.swift
@@ -16,12 +16,33 @@ struct MainView: View {
         NavigationView{
             List{
                 ForEach(vModel.listContent) {employee in
-                    Text("\(employee.name) :: \(employee.job)")
-                }
+                    //Table View Content
+                    ZStack(alignment: .leading){
+                        RoundedRectangle(cornerRadius: 20.0)
+                            .fill(Color.green)
+                            .frame(height: 100)
+                        VStack(alignment: .leading){
+                            Text("Employee : \(employee.name)")
+                                .font(.title2)
+                                .padding(.bottom , 10)
+                            Text("Job-Title   : \(employee.job)")
+                                .font(.title2)
+                                .padding(.bottom,10)
+                        }
+                        .padding()
+                        //VStack
+                    }
+                    .padding(-10)
+                    //ZStack
+                }//ForEach
             }
+            .navigationTitle(Text("Employees"))
+            .padding(EdgeInsets(top: 0
+                                , leading: -20, bottom: 0, trailing: -20))
         }
+ 
     }
-    
+       
     //Data Fetching on Load App View
     init(){
         vModel.LoadData()
