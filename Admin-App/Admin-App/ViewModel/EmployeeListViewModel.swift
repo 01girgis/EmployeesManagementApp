0ae84abd4fdce330ee2fdb667a39055cadbd5761
crus <romanghiet05@gmail.com>
2022-06-13 04:57:53 -0700

    Data Fecthing function for Admin app

--- Admin-App/Admin-App/ViewModel/EmployeeListViewModel.swift
+++ Admin-App/Admin-App/ViewModel/EmployeeListViewModel.swift
@@ -13,6 +13,18 @@ class EmployeeListViewModel:ObservableObject{
     @Published var listContent = [Employee] ()
     
     func LoadData(){
-
+        //Database Connection
+        let dbInstance = Firestore.firestore()
+        
+        //Data Collection Selection
+        dbInstance.collection("Employees").addSnapshotListener{ (snapShot , error) in
+            //Error Checking
+            guard let snapshotUpdater = snapShot?.documents else {
+                print("\(error!.localizedDescription)")
+                return
+            }
+            
+       
+        }
     }
 }
